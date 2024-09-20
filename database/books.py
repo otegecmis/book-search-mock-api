b4b2e52868c029c810c70af3c51ee8169c1dea9f
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-09-21 01:32:20 +0300

    chore: add more book `mock data`

--- database/books.py
+++ database/books.py
@@ -48,5 +48,95 @@ books = [
         "published": "1851",
         "isbn13": "9781503280786",
         "isbn10": "1503280780"
+    },
+    {
+        "id": "9f820564-df28-4f46-9b43-4b7d1e3293fb",
+        "title": "Pride and Prejudice",
+        "author": "Jane Austen",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/PrideAndPrejudiceTitlePage.jpg/800px-PrideAndPrejudiceTitlePage.jpg",
+        "publisher": "T. Egerton, Whitehall",
+        "published": "1813",
+        "isbn13": "9781503290563",
+        "isbn10": "1503290565"
+    },
+    {
+        "id": "2b7b07cf-d292-41b3-b0ea-2632f17c9236",
+        "title": "The Catcher in the Rye",
+        "author": "J. D. Salinger",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/The_Catcher_in_the_Rye_%281951%2C_first_edition_cover%29.jpg/774px-The_Catcher_in_the_Rye_%281951%2C_first_edition_cover%29.jpg",
+        "publisher": "Little, Brown and Company",
+        "published": "1951",
+        "isbn13": "9780316769488",
+        "isbn10": "0316769487"
+    },
+    {
+        "id": "4d39683a-7865-4710-a72a-0509a5ec6223",
+        "title": "Brave New World",
+        "author": "Aldous Huxley",
+        "image": "https://upload.wikimedia.org/wikipedia/en/6/62/BraveNewWorld_FirstEdition.jpg",
+        "publisher": "Chatto & Windus",
+        "published": "1932",
+        "isbn13": "9780060850524",
+        "isbn10": "0060850523"
+    },
+    {
+        "id": "b9f4bb49-22e2-46f7-9fe7-1b09d4a2dc4b",
+        "title": "The Lord of the Rings: The Fellowship of the Ring",
+        "author": "J. R. R. Tolkien",
+        "image": "https://upload.wikimedia.org/wikipedia/en/8/8e/The_Fellowship_of_the_Ring_cover.gif",
+        "publisher": "George Allen & Unwin",
+        "published": "1954",
+        "isbn13": "9780618640157",
+        "isbn10": "0618640150"
+    },
+    {
+        "id": "7a1ac06d-c5be-4a1e-9a3f-558c87795ac5",
+        "title": "Crime and Punishment",
+        "author": "Fyodor Dostoevsky",
+        "image": "https://upload.wikimedia.org/wikipedia/en/4/4b/Crimeandpunishmentcover.png",
+        "publisher": "The Russian Messenger",
+        "published": "1866",
+        "isbn13": "9780140449136",
+        "isbn10": "0140449132"
+    },
+    {
+        "id": "4f6d1b2d-4478-4195-8f43-4cbbf49ed1d7",
+        "title": "Jane Eyre",
+        "author": "Charlotte Brontë",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Jane_Eyre_title_page.jpg/330px-Jane_Eyre_title_page.jpg",
+        "publisher": "Smith, Elder & Co.",
+        "published": "1847",
+        "isbn13": "9780141441146",
+        "isbn10": "0141441143"
+    },
+    {
+        "id": "30f48d89-bb36-46c7-b1b0-f726017aa226",
+        "title": "Anna Karenina",
+        "author": "Leo Tolstoy",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/AnnaKareninaTitle.jpg/330px-AnnaKareninaTitle.jpg",
+        "publisher": "The Russian Messenger",
+        "published": "1877",
+        "isbn13": "9780143035008",
+        "isbn10": "0143035002"
+    },
+    {
+        "id": "d12d5e22-9fc8-49ef-b54a-f6f13b0cf36d",
+        "title": "The Odyssey",
+        "author": "Homer",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Odyssey-crop.jpg/330px-Odyssey-crop.jpg",
+        "publisher": "Ancient Greece",
+        "published": "8th century BC",
+        "isbn13": "9780143039952",
+        "isbn10": "0143039954"
+    },
+    {
+        "id": "bba5fd95-e86e-44ea-942a-6a45e479da4e",
+        "title": "War and Peace",
+        "author": "Leo Tolstoy",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Tolstoy_-_War_and_Peace_-_first_edition%2C_1869.jpg/330px-Tolstoy_-_War_and_Peace_-_first_edition%2C_1869.jpg",
+        "publisher": "The Russian Messenger",
+        "published": "1869",
+        "isbn13": "9780140447934",
+        "isbn10": "0140447938"
     }
 ]
