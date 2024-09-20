5f2cfdcce7064ec3b8f95a62bf203dfb3b2c0c20
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-09-20 22:56:20 +0300

    feat: create a `project`

---
+++ database/books.py
@@ -0,0 +1,52 @@
+books = [
+    {
+        "id": "ee3b0649-991a-4e12-8c6e-626311d3491e",
+        "title": "Harry Potter and the Philosopher's Stone",
+        "author": "J. K. Rowling",
+        "image": "https://upload.wikimedia.org/wikipedia/en/6/6b/Harry_Potter_and_the_Philosopher%27s_Stone_Book_Cover.jpg",
+        "publisher": "Bloomsbury Publishing",
+        "published": "1997",
+        "isbn13": "9780747532699",
+        "isbn10": "0747532699"
+    },
+    {
+        "id": "cd52fa02-5019-43be-87b6-7bc59753c5ed",
+        "title": "To Kill a Mockingbird",
+        "author": "Harper Lee",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg/330px-To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg",
+        "publisher": "J.B. Lippincott & Co.",
+        "published": "1960",
+        "isbn13": "9780060935467",
+        "isbn10": "0060935464"
+    },
+    {
+        "id": "fc929c5e-5d65-4e7f-bbbf-b564a963f2c4",
+        "title": "1984",
+        "author": "George Orwell",
+        "image": "https://upload.wikimedia.org/wikipedia/en/5/51/1984_first_edition_cover.jpg",
+        "publisher": "Secker & Warburg",
+        "published": "1949",
+        "isbn13": "9780451524935",
+        "isbn10": "0451524934"
+    },
+    {
+        "id": "ff3b41db-70b2-4f35-9ec6-cdfe16432c13",
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/The_Great_Gatsby_Cover_1925_Retouched.jpg/1024px-The_Great_Gatsby_Cover_1925_Retouched.jpg",
+        "publisher": "Charles Scribner's Sons",
+        "published": "1925",
+        "isbn13": "9780743273565",
+        "isbn10": "0743273567"
+    },
+    {
+        "id": "83461eae-eacd-4e65-90d2-733963a1e491",
+        "title": "Moby Dick",
+        "author": "Herman Melville",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Moby-Dick_FE_title_page.jpg/800px-Moby-Dick_FE_title_page.jpg",
+        "publisher": "Harper & Brothers",
+        "published": "1851",
+        "isbn13": "9781503280786",
+        "isbn10": "1503280780"
+    }
+]
