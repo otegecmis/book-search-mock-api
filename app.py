594176c6793dfb7a14b263a5b1f1f147a13e8baa
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-09-21 01:33:57 +0300

    feat: create a `mock API` (#1)

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+from database.books import books
+
+app = Flask(__name__)
+
+@app.route('/books/search/<string:isbn>', methods=['GET'])
+def search_book(isbn):
+    for book in books:
+        if book['isbn13'] == isbn or book['isbn10'] == isbn:
+            return jsonify(book)
+
+    return jsonify({"error": "Book not found"}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
